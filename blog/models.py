86bb70773e22a30b64472e8ac63b8084c7f1830a
arthur <arthurdolival@gmail.com>
2025-02-24 11:34:13 -0300

    feat: blog views added
    - implemented a view where all posts are displayed
    - implemented a view for detailed post information
    - made SEO-friendly URL for posts

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,25 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
+    objects = models.Manager()
+    published = PublishedManager()
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255)
+    slug = models.SlugField(
+        max_length=255,
+        unique_for_date='publish'
+    )
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -24,11 +35,25 @@ class Post(models.Model):
         default=Status.DRAFT
     )
 
+
     class Meta:
         ordering = ['-publish']
         indexes = [
             models.Index(fields=['-publish'])
         ]
 
+
     def __str__(self):
         return self.title
+    
+
+    def get_absolute_url(self):
+        return reverse(
+            'blog:post_detail',
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug
+            ]
+        )
