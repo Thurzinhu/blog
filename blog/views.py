4960d430c11810b62d630b4e99b4a29312484eaf
arthur <arthurdolival@gmail.com>
2025-02-28 12:16:33 -0300

    feat: text search functionality implemented
    - Using PostgreSQL full text search engine to make
    searchs more accurate

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
@@ -5,7 +6,7 @@ from django.views.decorators.http import require_POST
 from django.core.mail import send_mail
 from django.db.models import Count
 from taggit.models import Tag
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from .models import Post
 
 
@@ -14,6 +15,39 @@ class PostListView(ListView):
     context_object_name = 'posts'
     paginate_by = 3
     template_name = 'blog/post/list.html'
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = (
+                SearchVector('title', weight='A', config='portuguese') +
+                SearchVector('body', weight='B', config='portuguese')
+            )
+            search_query = SearchQuery(query, config='portuguese')
+            results = (
+                Post.published.annotate(
+                    search=search_vector,
+                    rank=SearchRank(search_vector, search_query)
+                )
+                .filter(rank__gte=0.3)
+                .order_by('-rank')
+            )
+    return render(
+        request,
+        'blog/post/search.html',
+        {
+            'form': form,
+            'query': query,
+            'results': results
+        }
+    )
 
 
 def post_share(request, post_id):
